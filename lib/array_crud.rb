f5a9887d3d0133e1d8810ebf3f0d9af02b7f238b
Nyasha Perry <nyashaperry2005@yahoo.com>
2020-12-19 19:12:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
  grocery_list = ["eggs", "bacon", "cheese", "bread"]
- grocery_list
+ grocery_list << "juice"
 end
 
 def add_element_to_start_of_array(array, element)
