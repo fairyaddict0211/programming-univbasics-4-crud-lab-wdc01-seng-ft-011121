d5048852b0e9797f3cdc3de4326cf5282803aedb
Nyasha Perry <nyashaperry2005@yahoo.com>
2020-12-19 19:10:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  grocey_list << "juice"
+  grocery_list << ("juice")
 end
 
 def add_element_to_start_of_array(array, element)
